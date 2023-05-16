a79e8b4ff1028a3ca28068ee9300a5bc022fc437
IDaneva <daneva_n@abv.bg>
2023-05-16 15:00:55 +0300

    made a basic window

---
+++ white_board.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter.colorchooser import askcolor
+from tkinter import ttk
+import tkinter as tk
+
+
+# making a basic screen, which is not resizable because otherwise there will be issues with all the draws on the board
+root = Tk()
+root.title("White board")
+root.geometry("1050x570+150+50")
+root.configure(bg="#f2f3f5")
+root.resizable(False, False)
+
+# make the window startable
+root.mainloop()
