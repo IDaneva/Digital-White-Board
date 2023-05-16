3aea3f55dca763a1d8ebc0e3a42f48907e571316
IDaneva <daneva_n@abv.bg>
2023-05-16 15:16:26 +0300

    set a placeholder for the colors on the side

--- white_board.py
+++ white_board.py
@@ -11,5 +11,14 @@ root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False, False)
 
+# set an icon for the window
+image_icon = PhotoImage(file= "whiteboard.png")
+root.iconphoto(False, image_icon)
+
+# set a label for the colors on the side
+color_box = PhotoImage(file="color box.png")
+Label(root, image=color_box, bg="#f2f3f5").place(x=10, y=20)
+
+
 # make the window show up
 root.mainloop()
