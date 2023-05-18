cadb2cecdfee7a2d938aed1d96d54a773dae249f
IDaneva <daneva_n@abv.bg>
2023-05-18 16:15:43 +0300

    added erase and draw function

--- white_board.py
+++ white_board.py
@@ -11,6 +11,35 @@ root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False, False)
 
+current_x = 0
+current_y = 0
+color = "black"
+
+
+def locate_xy(work):
+
+    global current_x, current_y
+    current_x = work.x
+    current_y = work.y
+
+
+def add_line(work):
+    global current_x, current_y
+    canvas.create_line((current_x, current_y, work.x, work.y), width=2, fill=color)
+    current_x = work.x
+    current_y = work.y
+
+
+def show_color(new_color):
+    global color
+    color = new_color
+
+
+def new_canvas():
+    canvas.delete("all")
+    display_palette()
+
+
 # set an icon for the window
 image_icon = PhotoImage(file= "whiteboard.png")
 root.iconphoto(False, image_icon)
@@ -23,34 +52,35 @@ Label(root, image=color_box, bg="#f2f3f5").place(x=10, y=20)
 colors = Canvas(root, bg="#ffffff", width=37, height=300, bd=0)
 colors.place(x=20, y=50)
 
+
 # set the colors for the palette
 def display_palette():
     id = colors.create_rectangle((10, 10, 30, 30), fill="black")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("black"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("black"))
 
     id = colors.create_rectangle((10, 40, 30, 60), fill="gray")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("gray"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("gray"))
 
     id = colors.create_rectangle((10, 70, 30, 90), fill="brown4")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("brown4"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("brown4"))
 
     id = colors.create_rectangle((10, 100, 30, 120), fill="red")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("red"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("red"))
 
     id = colors.create_rectangle((10, 130, 30, 150), fill="orange")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("orange"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("orange"))
 
     id = colors.create_rectangle((10, 160, 30, 180), fill="yellow")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("yellow"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("yellow"))
 
     id = colors.create_rectangle((10, 190, 30, 210), fill="green")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("green"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("green"))
 
     id = colors.create_rectangle((10, 220, 30, 240), fill="blue")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("blue"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("blue"))
 
     id = colors.create_rectangle((10, 250, 30, 270), fill="purple")
-    colors.tag_bind(id, "Button-1", lambda x: show_color("purple"))
+    colors.tag_bind(id, "<Button-1>", lambda x: show_color("purple"))
 
 
 display_palette()
@@ -58,14 +88,14 @@ display_palette()
 
 # set the eraser for the white board
 eraser = PhotoImage(file="cartoon-eraser-icon-png.png")
-Button(root, image=eraser, bg="#f2f3f5").place(x=20, y=400)
+Button(root, image=eraser, bg="#f2f3f5", command=new_canvas).place(x=20, y=400)
 
 # set the canvas place for drawing
 canvas = Canvas(root, width=930, height=500, bg="white", cursor="hand2")
 canvas.place(x=100, y=10)
 
 canvas.bind("<Button-1>", locate_xy)
-canvas.bind("<B1-Motion>", addLine)
+canvas.bind("<B1-Motion>", add_line)
 
 
 # make the window show up
